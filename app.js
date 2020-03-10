f5ee4b2c0a023dc50cd861e280b3e1e05b8fbe26
Brian Emilius <brian@brianemilius.com>
2020-03-10 07:47:17 +0100

    add cors support

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var app = express();
 var router = require("./router");
 var { testConnection } = require("./config/database");
 var formidable = require("express-formidable");
+var cors = require("cors");
 
 testConnection();
+app.use(cors());
 app.use(formidable());
 app.use(router);
 
